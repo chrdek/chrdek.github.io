f9ff85b6b9bc22b01ffc69b0a3a9fab3b78de217
chrdek <cdekatrr@hotmail.com>
2023-10-22 01:45:12 +0300

    changesets etc.

---
+++ scripts/_reqs.js
@@ -0,0 +1,10 @@
+/*(function(url) {
+ const xhr = new XMLHttpRequest()
+ xhr.open( 'GET','https://chrdek.github.io');
+ xhr.setRequestHeader('Access-Control-Allow-Headers', '*');
+xhr.setRequestHeader('Content-Security-Policy-Report-Only','default-src https:; report-to /csp-violation-report-endpoint/');
+ xhr.withCredentials = false;
+ xhr.onreadystatechange = function() {
+    console.log("presetting uri ok")
+}
+})();*/
