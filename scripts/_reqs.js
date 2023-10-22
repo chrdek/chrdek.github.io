0afe0e3821ad439ce65d140fbec04ffc3bd5c6e7
chrdek <cdekatrr@hotmail.com>
2023-10-22 03:42:01 +0300

    small changes .

--- scripts/_reqs.js
+++ scripts/_reqs.js
@@ -1,10 +1,10 @@
 (function() {
  const xhr = new XMLHttpRequest()
- xhr.open( 'POST','http://logs-01.loggly.com/inputs/ab48807b-4cfb-452e-8371-f9eab2134e32/tag/http/');
+ xhr.open( 'POST','https://logs-01.loggly.com/inputs/ab48807b-4cfb-452e-8371-f9eab2134e32/tag/http/');
  xhr.setRequestHeader('Access-Control-Allow-Headers', '*');
  xhr.setRequestHeader('Access-Control-Allow-Methods','GET,POST,OPTIONS,PUT');
  xhr.setRequestHeader('Content-type','application/reports+json');
- xhr.setRequestHeader('Reporting-Endpoints','main-endpoint="http://logs-01.loggly.com/inputs/ab48807b-4cfb-452e-8371-f9eab2134e32/tag/http/"');
+ xhr.setRequestHeader('Reporting-Endpoints','main-endpoint="https://logs-01.loggly.com/inputs/ab48807b-4cfb-452e-8371-f9eab2134e32/tag/http/"');
  xhr.withCredentials = false;
  xhr.onreadystatechange = function() {
     console.log("presetting uri ok")
