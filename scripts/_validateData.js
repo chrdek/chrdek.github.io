aef4bc2cbd08d8611936998c9d1b3bad0eb573b7
chrdek <cdekatrr@hotmail.com>
2023-11-03 02:55:42 +0200

    __

--- scripts/_validateData.js
+++ scripts/_validateData.js
@@ -1,11 +1,9 @@
 class ValidateInput {
-
  constructor(response_val) {
    this.testdata = response_val;
    this.jsonPattern = '/(\|{\[){1,}|(\}|\]){1,}/g';
-   isXmlorJs() { return Boolean((new DOMParser().parseFromString(this.testdata,'application/xml').querySelector('parsererror') != undefined) & (JSON.stringify(this.testdata).match(this.jsonPattern) != null))
-? 'JSON' : 'XML' }
+   this.isXmlorJs = () => Boolean((new DOMParser().parseFromString(this.testdata,'application/xml').querySelector('parsererror') != undefined) & (JSON.stringify(this.testdata).match(this.jsonPattern) != null))
+? 'JSON' : 'XML';
 
  }
-
 }
