754b03dc9a6ef65cf4b00b6a32600c84afd1692c
chrdek <cdekatrr@hotmail.com>
2023-11-06 03:11:37 +0200

    other file changes.

--- scripts/_validateData.js
+++ scripts/_validateData.js
@@ -1,2 +1,16 @@
-const isXmlorJs = (res) => Boolean((new DOMParser().parseFromString(res,'application/xml').querySelector('parsererror') != undefined) || (JSON.stringify(res).match(/(\|{\[){1,}|(\}|\]){1,}/g) != null))
-? 'JSON' : 'XML'; 
+$(document).ready(function() {
+
+//retrieve full content etc.
+let innerContent = document.all; let textpart = "";
+$.each(innerContent, (key,value) => {
+textpart += innerContent[key].outerHTML;
+ });
+
+//validation on DOM.
+(() => {
+return (new DOMParser().parseFromString(textpart,'text/xml').documentElement.querySelector('parsererror')) == undefined 
+? console.log("DOMContentLoaded produces XTHML valid parts.")
+: console.log("DOMContentLoaded XHTML invalid! check file.");
+ })(textpart);
+
+});
