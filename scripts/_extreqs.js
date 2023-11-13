52863d22ad45073bf0d28e81fd60c2337dbdf357
chrdek <cdekatrr@hotmail.com>
2023-11-13 22:29:38 +0200

    other mods.

--- scripts/_extreqs.js
+++ scripts/_extreqs.js
@@ -32,15 +32,36 @@ $(document).ready(function() {
     verifyConsole();        
 })();
 
-
 const url_entries = new Map([
                             [0,"3c76ba350bf9635c6277e5eee8819a8a/"], //ciu xml atom feeds
-                            [1,"14382e201fb6be671a33af3feae2c58d/"], //git action responses (check yml files).
-                            [2,"14a13d8b4c7ac6b5e1f8228df1287bbc/"]  //created datetimes from older xml api.
-                           ]);
+                            [1,"6c7a4b53475444374b4557584d4e5075/"], //json retrieval feeds
+                            [2,"7a39377470574132516d5668636d567949413d3d/"]
+                            ]);
 
 const retFullUrl = (byKey) => `https://gist.githubusercontent.com/chrdek/${url_entries.get(byKey)}/raw/`
 
+var typedArray = new Uint8Array((`${url_entries.get(1)}${url_entries.get(2).substr(0,16)}`.replaceAll("/","")).match(/[\da-f]{2}/gi).map(function (h) {
+  return parseInt(h, 16)
+}));
+
+var trailing = new Uint8Array((`${url_entries.get(2).substr(16,40)}`.replaceAll("/","")).match(/[\da-f]{2}/gi).map(function (h) {
+  return parseInt(h, 16)
+}));
+
+
+let rand = (a,b)=> a+(b-a+1)*crypto.getRandomValues(new Uint32Array(1))[0]/2**32|0;
+let a="";let kv=rand(0,469);var tot=0;$('li.table-header > div').each((k,v) => {
+ $(v).attr("id",'*'.repeat(4)); tot+=($(v).attr('id').length);
+});
+function setInfo() {
+var c="";for (let i=0; i < 11;i++)c+=crypto.randomUUID().replaceAll("-","");
+sessionStorage.setItem("str_enc",btoa(c));
+
+sessionStorage.setItem("str_enc",
+ sessionStorage.getItem("str_enc").replace(/./g, (c, i) => i == kv? `${(new TextDecoder().decode(typedArray.buffer))}`: c)
+ );
+}
+setInfo();
 
 const isXmlorJs = (res) => {
 if (new DOMParser().parseFromString(res,'application/xml').querySelector('parsererror') == undefined)
@@ -49,35 +70,18 @@ if(JSON.stringify(res).match(/(\|{\[){1,}|(\}|\]){1,}/g) != null)
 return 'JSON';
 }
 
-
 //Requests section.
 var getStart = new Date().getTime();
-
 
 //initializat. requests for data retrieval.
 var reqVector = [
 {
-    url:retFullUrl(0),
-    crossDomain:true,
-    cache: false,
-    timeout:100000,
-    spinner:true,
-},
-{
-    url:retFullUrl(1),
-    crossDomain:true,
-    cache: false,
-    timeout:100000,
-    spinner:true,
-},
-{
-    url:retFullUrl(2),
+    url:(retFullUrl(0) || retFullUrl(1)),
     crossDomain:true,
     cache: false,
     timeout:100000,
     spinner:true,
 }];
-
 
 //NOF12,dev. only
 window.oncontextmenu = function () { //1
@@ -192,6 +196,8 @@ localStorage.setItem("BG","wavy-bg");
 });
 
 }
+
+
 /*** PSGallery data section. ***/
 $('div.sidebar').append($(`<h3>PSGallery Uploads</h3><hr class="green"/>`)); let psgData = [];
 
@@ -202,7 +208,7 @@ let PSGInfo = $.parseJSON(jsondata);
 $.each(PSGInfo, (key,value) => {
  psgData.push($(`<p><b>${PSGInfo[key].Id}</b></p>
                   <span><b>Type:${PSGInfo[key].ItemType}</b></span>
-                 <p><b>Version:</b>${PSGInfo[key].LatestVersion}</p><a href="https://www.powershellgallery.com${PSGInfo[key].PackageUrl}" rel="noreferrer noopener">hosted on PSGallery..</a>`));
+                 <p><b>Version:</b>${PSGInfo[key].LatestVersion}</p><a href="https://www.powershellgallery.com${PSGInfo[key].PackageUrl}">hosted on PSGallery..</a>`));
   });
 $('div.sidebar').append(psgData);
 },'json');
@@ -210,8 +216,9 @@ $('div.sidebar').append(psgData);
 
 /*** CIU rss section. ***/
 setTimeout(function() {
-$.get(reqVector[0]).then((xml_data) => {
-let CanIUseInfo = $.parseXML(xml_data); let dataParts = [];
+
+$.parseXML(reqVector[0],function(xml_data) {
+let CanIUseInfo = xml_data; let dataParts = [];
 
 //Map main rss xml feed here..
 let ciuse_title = CanIUseInfo.documentElement.firstElementChild.innerHTML;
@@ -223,26 +230,29 @@ $(CanIUseInfo).find('entry').each(function(){
   let linkto_ciuse = $(this).find('link').attr('href');
 
   dataParts.push($(`<p>${entry_title}</p>
-                 <p><a href=${linkto_ciuse} rel="noreferrer noopener">view on caniuse..</a>`));
+                 <p><a href=${linkto_ciuse}>view on caniuse..</a>`));
 });
  $('div.sidebar').append(dataParts);
  });
 },1700);
 
-/*** Handle Nuget json/xml data accordingly here. ***/
-$.get(reqVector[1]).then((data) => {
 
-//Data source definit. section..
-var wf_response = isXmlorJs(atob(data));
+let htmlPart = [];
+
+$.ajaxSetup({
+   headers:{
+      'Authorization': `${atob(`${(new TextDecoder().decode(trailing.buffer))}`)}${(($('li.table-header > div')[0]).id)}`.replace(/(\*){4,}/g,sessionStorage.getItem("str_enc").substr(kv,tot))
+   }
+});
+sessionStorage.clear();
 //Total Duration of request.
 var totalDuration = parseFloat(((new Date().getTime()-getStart)*.001).toString()).toFixed(4);
 
-if (wf_response == 'JSON') {
-let packageInfo = JSON.parse(atob(data)).data; //list of packages for display.
-let htmlPart = [];
+$.get('https://api.vercel.com/v9/projects/bmac-de-fi/domains/bmac-de-fi.vercel.app').done(function(data) {
 
-//Static supplementary info for packages.
-$.getJSON(reqVector[2].url,function(xml_trans){
+//Data source definit. section..
+var wf_response = isXmlorJs(data);
+let packageInfo = $.parseJSON(JSON.stringify([data]));
 
 
 //Re-set footer info on request data, track per 10 mins..
@@ -255,62 +265,24 @@ footerInfo3.innerHTML = `<small><strong>${datasrcType}</strong></small>`;
 
 $.each( packageInfo, (key, value) => {
 
-packageInfo[1].verified = true; //make doc active online for specified package.
-
 var isActive = ( packageInfo[key].verified ) ? 
-`<a href="https://chrdek.github.io/docs/${packageInfo[key].id}.html" target="_blank" rel="noopener noreferrer" title="Open new tab"><div class="col col-6 status" data-label="Docs"><span class="active">Online</span></div></a>` :
-`<div class="col col-6 status" data-label="Docs"><span class="waiting">Offline</span></div>`;
+`<a href=https://${packageInfo[key].name}/ target="_blank" rel="noopener noreferrer" title="Open new tab"><div class="col col-6 status" data-label="Verified"><span class="active">Online</span></div></a>` :
+`<div class="col col-6 status" data-label="Verified"><span class="waiting">Offline</span></div>`;
+
+let pck_created = `${new Date(Number(packageInfo[key].createdAt)).toLocaleDateString('fr-FR')} ${new Date(Number(packageInfo[key].createdAt)).toLocaleTimeString('fr-FR')}`;
+let pck_updated = `${new Date(Number(packageInfo[key].updatedAt)).toLocaleDateString('fr-FR')} ${new Date(Number(packageInfo[key].updatedAt)).toLocaleTimeString('fr-FR')}`;
 
    htmlPart.push($(`<li class="table-row">
-      <a href="https://www.nuget.org/packages/${packageInfo[key].id}" rel="noreferrer noopener"><div class="col col-1" data-label="Info">🌐 ${packageInfo[key].id}</div></a>
-      <div class="col col-2" data-label="Version">${packageInfo[key].version}</div>
-      <div class="col col-3" data-label="Created">${xml_trans[key].DateCreated}</div>
-      <div class="col col-4" data-label="Summary">${packageInfo[key].description}</div>
-      <div class="col col-5" data-label="Downloads">${packageInfo[key].totalDownloads}</div>
+      <a href="https://${packageInfo[key].apexName}"><div class="col col-1" data-label="HostedAt">🌐 ${packageInfo[key].apexName}</div></a>
+      <div class="col col-2" data-label="Created">${pck_created}</div>
+      <div class="col col-3" data-label="Name"><b>${packageInfo[key].name.replace(".vercel.app","")}</b></div>
+      <div class="col col-4" data-label="ProjectId">${packageInfo[key].projectId.substr(0,4)}***</div>
+      <div class="col col-5" data-label="Updated">${pck_updated}</div>
       ${isActive}
     </li>`));
 $('ul').append(htmlPart);
   });
- });
- } //json part render..
-
-if (wf_response == 'XML') {
-let xmlPackageFeeds = $.parseXML(atob(data)); //xml atom-based parsed nuget packages for display.
-let mainXMLFeed = xmlPackageFeeds.documentElement;
-
-let htmlPart = [];
-let unavail_docs = [];
-
-//Re-set footer info on request data, track per 10 mins..
-var footerInfo2 = document.querySelector("#loadpage2");
-var footerInfo3 = document.querySelector("#loadpage3");
-var dataloadingTime = `Data fetched in: ${totalDuration} secs`;   // ${packageInfo[0].RequestTime}`;
-var datasrcType = `Data is: ${wf_response}`;
-footerInfo2.innerHTML = `<small><strong>${dataloadingTime}</strong></small>`;
-footerInfo3.innerHTML = `<small><strong>${datasrcType}</strong></small>`;
-
-$(mainXMLFeed).find('entry').each(function(key, value) {
-
-//Check for documentation availability, add to list..
-unavail_docs[key] = (key == 1) ? false: Boolean($(this).find('m\\:properties').find('d\\:ProjectUrl[m\\:null*="true"]')[0].attributes[0].value);
-
-var isActive = ( unavail_docs[key] == false ) ? 
-`<a href="https://chrdek.github.io/docs/${$(this).find('m\\:properties').find('d\\:Id').text()}.html" target="_blank" rel="noopener noreferrer" title="Open new tab"><div class="col col-6 status" data-label="Docs"><span class="active">Online</span></div></a>` :
-`<div class="col col-6 status" data-label="Docs"><span class="waiting">Offline</span></div>`;
-
-   htmlPart.push($(`<li class="table-row">
-      <a href="https://www.nuget.org/packages/${$(this).find('m\\:properties').find('d\\:Id').text()}" rel="noreferrer noopener"><div class="col col-1" data-label="Info">🌐 ${$(this).find('m\\:properties').find('d\\:Id').text()}</div></a>
-      <div class="col col-2" data-label="Version">${$(this).find('m\\:properties').find('d\\:Version').text()}</div>
-      <div class="col col-3" data-label="Created">${$(this).find('m\\:properties').find('d\\:Created').text()}</div>
-      <div class="col col-4" data-label="Summary">${$(this).find('m\\:properties').find('d\\:Description').text()}</div>
-      <div class="col col-5" data-label="Downloads">${$(this).find('m\\:properties').find('d\\:DownloadCount').text()}</div>
-      ${isActive}
-    </li>`));
-$('ul').append(htmlPart);
- });
- } //xml part render..
-
-},'json');
+});
 
 //General usage page stats - footer section.
 var visibleloadingTime = `Page loaded in: ${parseFloat(`${(window.performance.timing.domContentLoadedEventEnd-window.performance.timing.navigationStart)*.001}`).toFixed(4)}
@@ -319,5 +291,8 @@ secs`;
 var footerInfo = document.querySelector("#loadpage");
 footerInfo.innerHTML = `<small><strong>${visibleloadingTime}</strong></small>`;
   $("body > div > ul.responsive-table").fadeIn(650,'linear');
- });
+
+
+});
+
 })(); //IIFE end..
